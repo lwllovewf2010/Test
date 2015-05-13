175de04dfcfe751b569f598d88057f7ce8caab85
Allen <allen@allen-ThinkPad-E520.(none)>
2015-05-13 08:41:54 +0800

    flashlight ok

--- src/com/allen/test/tool/flashlight/FlashLight.java
+++ src/com/allen/test/tool/flashlight/FlashLight.java
@@ -16,128 +16,145 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.allen.test.R;
+import com.allen.test.tool.flashlight.FlashLightService.FlashLightCallback;
 import com.allen.test.tool.flashlight.FlashLightService.LocalBinder;
 
 public class FlashLight extends Activity {
 	final static String TAG = "TEST/FlashLight";
 
-	private ImageView mBg=null;
-	private ServiceConnection mSc; 
+	private ImageView mBg = null;
+	private ServiceConnection mSc;
 	private AlertDialog mPop;
 	private FlashLightService mService;
 	private View touchArea = null;
 	private View settingBtton;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.flashlight_activity);
+		Log.d(TAG, "onCreate");
 		
+		setContentView(R.layout.flashlight_activity);
+
 		mBg = (ImageView) findViewById(R.id.light_bg);
 		touchArea = findViewById(R.id.touch_area);
 		touchArea.setOnTouchListener(new FlashLightListener());
 		settingBtton = findViewById(R.id.setting_button);
 		settingBtton.setOnTouchListener(new FlashLightListener());
-		
-		 mSc = new ServiceConnection(){  	  
 
-				@Override
-				public void onServiceConnected(ComponentName arg0, IBinder arg1) {
-					// TODO Auto-generated method stub
-					Log.d(TAG,"onServiceConnected");
-					mService = ((LocalBinder)arg1).getService(); 
-					turnOn(true);
-				}
+		mSc = new ServiceConnection() {
 
-				@Override
-				public void onServiceDisconnected(ComponentName arg0) {
-					// TODO Auto-generated method stub
-					Log.d(TAG,"onServiceDisconnected");
-					mService = null;
-				}  
-	        }; 
-	        
-		    Intent service = new Intent();
-		    service.setClass(this, FlashLightService.class);
-			this.bindService(service, mSc, Context.BIND_AUTO_CREATE);	
+			@Override
+			public void onServiceConnected(ComponentName arg0, IBinder arg1) {
+				// TODO Auto-generated method stub
+				Log.d(TAG, "onServiceConnected");
+				mService = ((LocalBinder) arg1).getService();
+				mService.setCallBack(callback);
+				turnOn(true);
+			}
+
+			@Override
+			public void onServiceDisconnected(ComponentName arg0) {
+				// TODO Auto-generated method stub
+				Log.d(TAG, "onServiceDisconnected");
+				mService = null;
+			}
+		};
+
+		Intent service = new Intent();
+		service.setClass(this, FlashLightService.class);
+		this.bindService(service, mSc, Context.BIND_AUTO_CREATE);
 	}
-	
-	
-	
+
 	@Override
 	protected void onStart() {
 		// TODO Auto-generated method stub
 		super.onStart();
-		
+		Log.d(TAG, "onStart");
 	}
-
-
 
 	@Override
 	protected void onDestroy() {
 		// TODO Auto-generated method stub
+		Log.d(TAG, "onDestroy");
 		this.unbindService(mSc);
 		super.onDestroy();
 
 	}
-	
-	private  void turnOn(boolean on){
-		if(mService !=null){
-		mService.turnOn(on);
-		if(on){
-			mBg.setImageResource(R.drawable.shou_on);
-		}else{
-			mBg.setImageResource(R.drawable.shou_off);
+
+	FlashLightCallback callback = new FlashLightCallback() {
+
+		@Override
+		public void onLightStateChange(boolean on) {
+			// TODO Auto-generated method stub
+			Log.d(TAG, "onLightStateChange="+on);
+			if (on) {
+				mBg.setImageResource(R.drawable.shou_on);
+			} else {
+				mBg.setImageResource(R.drawable.shou_off);
+			}
 		}
+
+	};
+
+	private void turnOn(boolean on) {
+		if (mService != null) {
+			mService.turnOn(on);
+			if (on) {
+				mBg.setImageResource(R.drawable.shou_on);
+			} else {
+				mBg.setImageResource(R.drawable.shou_off);
+			}
 		}
 	}
 
-	private class FlashLightListener implements View.OnTouchListener{
+	private class FlashLightListener implements View.OnTouchListener {
 
 		@Override
 		public boolean onTouch(View arg0, MotionEvent arg1) {
 			// TODO Auto-generated method stub
-			
-			if(arg1.getAction()!=MotionEvent.ACTION_DOWN){
+
+			if (arg1.getAction() != MotionEvent.ACTION_DOWN) {
 				return true;
 			}
-			if(arg0.getId() == R.id.touch_area){
-				turnOn(! mService.isOn());
-			}else if(arg0.getId() == R.id.setting_button){
+			if (arg0.getId() == R.id.touch_area) {
+				turnOn(!mService.isOn());
+			} else if (arg0.getId() == R.id.setting_button) {
 				showDialog();
 			}
 
 			return true;
 		}
-		
-	}
-	
-	
 
-	
-	void showDialog(){
+	}
+
+	void showDialog() {
 		String[] items = new String[1];
 		boolean[] checkedItems = new boolean[1];
 		items[0] = "Enable float tool";
-		checkedItems[0]=false;
-		if(mPop!=null){
+		checkedItems[0] = mService.isFloatShow();
+		if (mPop != null) {
 			mPop.dismiss();
 		}
 		mPop = new AlertDialog.Builder(this)
-			.setMultiChoiceItems(items, checkedItems, PopListener)
-			.setPositiveButton("确定", null).create();
-					 		
+				.setMultiChoiceItems(items, checkedItems, PopListener)
+				.setPositiveButton("确定", null).create();
+
 		mPop.show();
 	}
-	
-	OnMultiChoiceClickListener PopListener= new OnMultiChoiceClickListener(){
+
+	OnMultiChoiceClickListener PopListener = new OnMultiChoiceClickListener() {
 
 		@Override
 		public void onClick(DialogInterface arg0, int arg1, boolean arg2) {
 			// TODO Auto-generated method stub
-			
+			Log.d(TAG, "PopListener arg1="+arg1+" arg2="+arg2);
+			if(mService!=null && arg1 ==0){
+				mService.showFloatTool(arg2);
+			}
+			mPop.dismiss();
 		}
-		
-	};
 
+	};
 
 }
--- src/com/allen/test/tool/flashlight/FlashLightService.java
+++ src/com/allen/test/tool/flashlight/FlashLightService.java
@@ -3,28 +3,51 @@ package com.allen.test.tool.flashlight;
 import com.allen.test.R;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.os.Binder;
 import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class FlashLightService extends Service {
 	
 	final static String TAG = "TEST/FlashLightService";
 	private Camera mCameraDevices;
 	private Camera.Parameters mParameters; 
-	boolean ison = false;
+	private boolean ison = false;
 	private Handler mHandler = new Handler();
-	
+	private FlashLightCallback mCallback;
+	private WindowManager mWindowManager;
+	private WindowManager.LayoutParams mWindowParams;
+	private int screenWidth;
+	private int screenHeight;
+	private int statusBarHeight;
+	static final int CUSTOM_LOCATION_X = 300;
+	static final int CUSTOM_LOCATION_Y = 800;
+	private float mLastX = CUSTOM_LOCATION_X;
+	private float mLastY = CUSTOM_LOCATION_Y;
+	boolean isDraging = true;
+	private ImageView floatView;
+	private int touchSlop;
+	private  boolean isShow=false;
 	@Override
 	public void onCreate() {
 		// TODO Auto-generated method stub
 		Log.d(TAG,"onCreate");
 		super.onCreate();
 		initCamera();
+		initFloatTool();
 	}
 
 	
@@ -36,6 +59,7 @@ public class FlashLightService extends Service {
 		if(mCameraDevices!=null){
 			mCameraDevices.release();
 		}
+		showFloatTool(false);
 		super.onDestroy();
 	}
 
@@ -80,6 +104,9 @@ public class FlashLightService extends Service {
 	public boolean isOn(){
 		return ison;
 	}
+	public boolean isFloatShow(){
+		return isShow;
+	}
 	
 	public  void turnOn(boolean on){
 		if(on){
@@ -89,6 +116,15 @@ public class FlashLightService extends Service {
 		}
 	}
 	
+	public void showFloatTool(boolean show){
+		if(show && ! isShow){
+			addFloatTool(CUSTOM_LOCATION_X,CUSTOM_LOCATION_Y);
+		}else if(!show && isShow){
+			hideFloatTool();
+		}
+	}
+	
+	
 	private Runnable onRunnable = new Runnable(){
 
 		@Override
@@ -97,12 +133,13 @@ public class FlashLightService extends Service {
 			if(mCameraDevices != null && mParameters!=null){
 				mParameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
 				mCameraDevices.setParameters(mParameters); 
-
 				ison = true;
+				
 			}else{
 				Log.d(TAG,"mParameters is null");
 				ison = true;
 			}
+			mCallback.onLightStateChange(ison);
 		}
 		
 	};
@@ -119,8 +156,125 @@ public class FlashLightService extends Service {
 				Log.d(TAG,"mParameters is null");
 				ison = true;
 			}
+			mCallback.onLightStateChange(ison);
 		}
 		
 	};
+	
+	interface FlashLightCallback{
+		void onLightStateChange( boolean on);
+	}
+	public void setCallBack(FlashLightCallback callback){
+		mCallback = callback;
+	}
+	
+	void initFloatTool(){
+		mWindowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+		mWindowParams = new WindowManager.LayoutParams();
+		
+		screenWidth = getResources().getDisplayMetrics().widthPixels;
+		screenHeight = getResources().getDisplayMetrics().heightPixels;
+		statusBarHeight = getStatusBarHeight();
+		
+		final ViewConfiguration configuration = ViewConfiguration.get(this);
+		touchSlop = configuration.getScaledTouchSlop();
+		
+		
+		floatView = (ImageView) LayoutInflater.from(this).inflate(
+				R.layout.flashlight_float, null);
+		floatView.setOnTouchListener(floatViewListener);
+		
 
+		
+	}
+	
+	private void addFloatTool(int x, int y) {
+		Log.d(TAG,"addFloatTool");
+		mWindowParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+		mWindowParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
+		mWindowParams.flags = 40;
+		mWindowParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+
+		mWindowParams.gravity = Gravity.LEFT | Gravity.TOP;
+		mWindowParams.x = x - floatView.getWidth() / 2;
+		mWindowParams.y = y - statusBarHeight - floatView.getHeight() / 2;
+		mWindowParams.format = PixelFormat.RGBA_8888;
+		mWindowManager.addView(floatView, mWindowParams);
+		isShow = true;
+	}
+	
+	private void hideFloatTool(){
+		Log.d(TAG,"hideFloatTool");
+		if (mWindowManager != null) {
+			mWindowManager.removeView(floatView);
+			isShow = false;
+		}
+	}
+	
+	private void updateFloatView(int x, int y) {
+		mWindowManager = (WindowManager) getApplicationContext()
+				.getSystemService("window");
+
+		mWindowParams.x = x - floatView.getWidth() / 2;
+		mWindowParams.y = y - statusBarHeight - floatView.getHeight() / 2;
+		mWindowManager.updateViewLayout(floatView, mWindowParams);
+	}
+	
+	View.OnTouchListener floatViewListener = new View.OnTouchListener() {
+
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			// TODO Auto-generated method stub
+			int mRawX = (int) event.getRawX();
+			int mRawY = (int) event.getRawY();
+
+			switch (event.getAction() & MotionEvent.ACTION_MASK) {
+			case MotionEvent.ACTION_DOWN:
+				mLastX = mRawX;
+				mLastY = mRawY;
+				isDraging = false;
+				break;
+			case MotionEvent.ACTION_MOVE:
+				if (isDraging || Math.abs(mRawX - mLastX) > touchSlop
+						|| Math.abs(mRawY - mLastY) > touchSlop) {
+					mLastX = mRawX;
+					mLastY = mRawY;
+					isDraging = true;
+					updateFloatView(mRawX, mRawY);
+				}
+				break;
+			case MotionEvent.ACTION_UP:
+			case MotionEvent.ACTION_CANCEL:
+				if (!isDraging) {
+					turnOn(! ison);
+				}
+				isDraging = false;
+				break;
+			default:
+				;
+			}
+
+			return true;
+		}
+		
+	};
+	
+	public int getStatusBarHeight() {
+		Class<?> c = null;
+		Object obj = null;
+		java.lang.reflect.Field field = null;
+		int x = 0;
+		int statusBarHeight = 0;
+		try {
+			c = Class.forName("com.android.internal.R$dimen");
+			obj = c.newInstance();
+			field = c.getField("status_bar_height");
+			x = Integer.parseInt(field.get(obj).toString());
+			statusBarHeight = getResources().getDimensionPixelSize(x);
+			return statusBarHeight;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return statusBarHeight;
+	}
 }
